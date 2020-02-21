4ee460dd0842fab2b90b8b621e956fd84eacfdb7
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-21 12:04:24 +0100

    Login && Signup almost done, Home started

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+import Navbar from './components/Navbar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import AuthRoute from './util/AuthRoute';
+
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login'
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+      <Router>
+       <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home}/>
+          <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
+            <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
